2e98883a779dc6234d473d5faa502376c19e0471
ANPA <ANPA@simcorp.com>
2019-02-15 10:59:57 +0100

    ckeditor.js - Add headerContent plugin to our content editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,13 +7,10 @@
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -28,19 +25,16 @@ import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import HeaderContent from './header-content/header-content';
 import '../css/highlight.css';
-export default class ClassicEditor extends ClassicEditorBase {}
+export default class ClassicEditor extends ClassicEditorBase { }
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
 	Autoformat,
 	Bold,
 	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
@@ -54,7 +48,9 @@ ClassicEditor.builtinPlugins = [
 	Table,
 	TableToolbar,
 	Highlight,
-	Alignment
+	Alignment,
+	BlockQuote,
+	HeaderContent
 ];
 
 // Editor configuration.
@@ -74,7 +70,8 @@ ClassicEditor.defaultConfig = {
 			'highlight',
 			'alignment',
 			'undo',
-			'redo'
+			'redo',
+			'headerContent'
 		]
 	},
 	highlight: {
@@ -124,6 +121,14 @@ ClassicEditor.defaultConfig = {
 			'mergeTableCells'
 		]
 	},
+	headerContent: {
+		items: [
+			{ tag: 'header-collapse-start', tagText: '[header-collapse-start]' },
+			{ tag: 'header-collapse-end', tagText: '[header-collapse-end]' },
+			{ tag: 'content-collapse', tagText: '[content-collapse]' }
+		]
+	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: 'en'
 };
+
